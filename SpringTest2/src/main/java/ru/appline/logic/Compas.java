d768b4e8e17626424665aa2d048bfef5b386fedd
AnnSafronova <63552669+AnnSafronova@users.noreply.github.com>
2023-04-24 23:00:01 +0400

    Add files via upload

---
+++ SpringTest2/src/main/java/ru/appline/logic/Compas.java
@@ -0,0 +1,39 @@
+package ru.appline.logic;
+
+public class Compas {
+
+
+    private String side;
+    private int startDegree;
+    private int endDegree;
+
+    public Compas(String side, int startDegree, int endDegree) {
+        this.side = side;
+        this.startDegree = startDegree;
+        this.endDegree = endDegree;
+    }
+
+    public void setSide(String side) {
+        this.side = side;
+    }
+
+    public void setStartDegree(int startDegree) {
+        this.startDegree = startDegree;
+    }
+
+    public void setEndDegree(int endDegree) {
+        this.endDegree = endDegree;
+    }
+
+    public String getSide() {
+        return side;
+    }
+
+    public int getStartDegree() {
+        return startDegree;
+    }
+
+    public int getEndDegree() {
+        return endDegree;
+    }
+}
